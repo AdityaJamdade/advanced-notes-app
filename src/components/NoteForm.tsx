1a0f2fcc401070b20ec401a3114730ac1a83cd34
AdityaJamdade <adijamdade156@gmail.com>
2023-10-22 12:37:16 +0530

    created types for noteform in app.tsx

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,16 +1,36 @@
+import { FormEvent, useRef, useState } from "react";
 import { Form, Col, Row, Stack, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
+import { NoteData, Tag } from "../App";
 
-export function NoteForm() {
+type NoteFormProps = {
+  onSubmit: (data: NoteData) => void;
+};
+
+export function NoteForm({ onSubmit }: NoteFormProps) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+
+  const submitHandler = (e: FormEvent) => {
+    e.preventDefault();
+
+    onSubmit({
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags : [],
+    })
+  };
+
   return (
-    <Form>
+    <Form onSubmit={submitHandler}>
       <Stack gap={4}>
         <Row>
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control required />
+              <Form.Control ref={titleRef} required />
             </Form.Group>
           </Col>
           <Col>
@@ -22,7 +42,7 @@ export function NoteForm() {
         </Row>
         <Form.Group controlId="markdown">
           <Form.Label>Body</Form.Label>
-          <Form.Control required as="textarea" rows={15} />
+          <Form.Control required as="textarea" ref={markdownRef} rows={15} />
         </Form.Group>
 
         <Stack direction="horizontal" gap={2} className="justify-content-end">
