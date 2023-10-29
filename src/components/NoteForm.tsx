9a32886401ce12eed22c60923827d94eabed2625
AdityaJamdade <adijamdade156@gmail.com>
2023-10-29 22:08:10 +0530

    edit note base functionality added, still to work a little

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -9,7 +9,7 @@ type NoteFormProps = {
   onSubmit: (data: NoteData) => void;
   onAddTag: (tag: Tag) => void;
   availableTags: Tag[];
-};
+} & Partial<NoteData>;
 
 export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
   const titleRef = useRef<HTMLInputElement>(null);
@@ -26,7 +26,7 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
       tags: selectedTags,
     });
 
-    navigate("..")
+    navigate("..");
   };
 
   return (
