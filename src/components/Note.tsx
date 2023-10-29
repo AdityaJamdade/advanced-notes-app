92f1cbb3913a993ce447950e1d5001d256ccb53b
AdityaJamdade <adijamdade156@gmail.com>
2023-10-29 22:20:20 +0530

    delete fn added

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,23 +1,27 @@
 import { Badge, Button, Col, Row, Stack } from "react-bootstrap";
 import ReactMarkdown from "react-markdown";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useNote } from "./NoteLayout";
 
-export function Note(){
-    const note = useNote()
-    
-    return <>
-    <Row className="align-items-center mb-4">
+type NoteProps = {
+  onDelete: (id: string) => void;
+};
+
+export function Note({ onDelete }: NoteProps) {
+  const note = useNote();
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Row className="align-items-center mb-4">
         <Col>
-        <h1>{note.title}</h1>
-        {note.tags.length > 0 && (
-            <Stack
-              gap={1}
-              direction="horizontal"
-              className="flex-wrap"
-            >
+          <h1>{note.title}</h1>
+          {note.tags.length > 0 && (
+            <Stack gap={1} direction="horizontal" className="flex-wrap">
               {note.tags.map((tag) => (
-                <Badge key={tag.id} className="text-truncate" >{tag.label}</Badge>
+                <Badge key={tag.id} className="text-truncate">
+                  {tag.label}
+                </Badge>
               ))}
             </Stack>
           )}
@@ -27,14 +31,22 @@ export function Note(){
             <Link to={`/${note.id}/edit`}>
               <Button variant="primary">Edit</Button>
             </Link>
-            <Button variant="outline-danger">Delete</Button>
+            <Button
+              variant="outline-danger"
+              onClick={() => {
+                onDelete(note.id);
+                navigate("/");
+              }}
+            >
+              Delete
+            </Button>
             <Link to="/">
               <Button variant="outline-secondary">Back</Button>
             </Link>
           </Stack>
         </Col>
-    </Row>
-    <ReactMarkdown>{note.markdown}</ReactMarkdown>
+      </Row>
+      <ReactMarkdown>{note.markdown}</ReactMarkdown>
     </>
-
+  );
 }
